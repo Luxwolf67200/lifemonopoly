98e39cee32de2d0ee42f69c1e14930f07b6a489c
Luxwolf67200 <lucas.verrier1@gmail.com>
2017-06-17 10:47:29 +0100

    casepremier

---
+++ case.cpp
@@ -0,0 +1,11 @@
+#include "case.h"
+#include <iostream>
+#include <QString>
+Case::Case()
+{
+std::cout << "constructeur class Case"<< std::endl;
+}
+Case::~Case()
+{
+  std::cout << "destructeur class Case"<< std::endl;
+}
---
+++ case.h
@@ -0,0 +1,18 @@
+#ifndef CASE_H
+#define CASE_H
+
+#include <QString>
+
+class Case
+{
+public:
+    Case();
+
+    virtual ~Case();
+    
+protected:
+    QString nom;
+
+};
+
+#endif // CASE_H
